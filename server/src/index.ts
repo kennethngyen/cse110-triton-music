a35753a9427883c6790f51126c0c6ef953a523e7
kennethngyen <kennyboy860@gmail.com>
2024-11-09 18:22:53 -0800

    db init push

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,18 @@
+import express from "express";
 import { Request, Response } from "express";
 import { createFeedEndpoints } from "./feed/feed-endpoints";
 import { createAccountEndpoints } from "./account/account-endpoints";
 import { feedItems, users } from "./constants";
 
-const express = require("express");
-const cors = require("cors");
 
 const app = express();
 const port = 8080;
 
-app.use(cors());
-app.use(express.json());
 
-// Start the server
-if (require.main === module) {
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
-}
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
 
 // Root endpoint to get test if the server is running
 app.get("/", (req: Request, res: Response) => {
