14bc64dc6db5854651c9b8fc543e63e981eab0d3
k3liang <130005349+k3liang@users.noreply.github.com>
2024-11-13 21:12:17 -0800

    fixed uniqueness of email error for database test

--- server/src/tests/DatabaseInit.test.ts
+++ server/src/tests/DatabaseInit.test.ts
@@ -1,3 +1,4 @@
+import { eq } from "drizzle-orm/sql";
 import { db } from "../db/db";
 import { usersTable } from "../db/schema";
 
@@ -6,8 +7,13 @@ describe("Add one user to User table, then get", () => {
         // const randString = (Math.random() + 1).toString(36).substring(7);
         const name = "Michael Coblenz";
         const email = "mcoblenz@ucsd.edu";
+
+        // to stop the UNIQUE error from the email
+        await db.delete(usersTable).where(eq(usersTable.email, email));
+
         const result = await db.insert(usersTable).values({ name, email }).returning();
-        console.log(result);
+        expect(result[0].name).toBe(name);
+        expect(result[0].email).toBe(email);
     });
     test("should pull users correctly", async () => {
         const allUsers = await db.select().from(usersTable);
